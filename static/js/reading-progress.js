96e90a18805ae5dbacb6aa2dc9024538eac944d7
ihateprogramming88 <AS12e@pm.me>
2024-07-06 14:07:57 +1000

    reading-progress bar update

--- static/js/reading-progress.js
+++ static/js/reading-progress.js
@@ -1,17 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
   var progressBar = document.getElementById('reading-progress-bar');
-  var body = document.body;
-  var html = document.documentElement;
+  var content = document.body;
+  var windowHeight = window.innerHeight;
+  var documentHeight = Math.max(
+    content.scrollHeight,
+    content.offsetHeight,
+    content.clientHeight
+  );
+  var readableLength = documentHeight - windowHeight;
 
-  function updateProgressBar() {
-    var scrollTop = body.scrollTop || html.scrollTop;
-    var height = html.scrollHeight - html.clientHeight;
-    var progress = (scrollTop / height) * 100;
-    progressBar.style.width = progress + '%';
-    progressBar.setAttribute('aria-valuenow', progress);
+  var lastKnownScrollPosition = 0;
+  var ticking = false;
+
+  function updateProgressBar(scrollPos) {
+    var progressPercentage = (scrollPos / readableLength) * 100;
+    progressBar.style.transform = `translateX(${progressPercentage - 100}%)`;
   }
 
-  window.addEventListener('scroll', updateProgressBar);
-  updateProgressBar();
-});
+  window.addEventListener('scroll', function(e) {
+    lastKnownScrollPosition = window.scrollY;
 
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        updateProgressBar(lastKnownScrollPosition);
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  });
+
+  window.addEventListener('resize', function() {
+    windowHeight = window.innerHeight;
+    documentHeight = Math.max(
+      content.scrollHeight,
+      content.offsetHeight,
+      content.clientHeight
+    );
+    readableLength = documentHeight - windowHeight;
+  });
+
+  updateProgressBar(window.scrollY);
+});
